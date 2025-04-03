44d50f9b8c7956da79072d52aceaff90c4626c09
Devansh <parekhdevansh2@gmail.com>
2025-04-03 12:41:16 +0530

    ok

--- 10.Rerverse.js
+++ 10.Rerverse.js
@@ -28,9 +28,9 @@ let temp = 0
 while (n != 0) {
 
     r = n % 10
-    rev=rev*10+r
-    n=Math.floor(n/10)
+    rev = rev * 10 + r
+    n = Math.floor(n / 10)
 }
-
-
 console.log(rev);
+
+
--- 2.EvenOrOddNumber.js
+++ 2.EvenOrOddNumber.js
@@ -1,10 +1,10 @@
 let num = 0;
 
 if (num % 2 == 0) {
-    console.log(num + " " + "THis is Even Number");
+    console.log(num + " " + "THis is Odd Number");
 
 
 } else if (num % 2 == 1) {
-    console.log(num + " " + "THis is Odd Number");
+    console.log(num + " " + "THis is Even Number");
 
 } 
